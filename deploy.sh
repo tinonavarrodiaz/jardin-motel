dcc7085db25cbd6ee0fc0790c4df4da3399b29de
Florentino Navarro <tino.navarro@hotmail.com>
2022-09-10 14:21:02 -0500

    deploy fixed

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:tinonavarrodiaz/jardin-motel.git main:gh-pages
+git push -f git@github.com:tinonavarrodiaz/moteljardin.git main:gh-pages
 
 cd -
